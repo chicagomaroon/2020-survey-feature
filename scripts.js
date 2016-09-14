19f892daf2bb15b0ab5c5ad7efd394826ce0f676
juliettehainline <juliette.hainline@gmail.com>
2016-09-13 22:56:35 -0400

    side nav bar works, needs to be responsive

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 /* Smooth Scrolling */
-/* Thanks https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+/* Thanks https://paulund.co.uk/smooth-scroll-to-internal-links-with-jquery */
 
-// $(function() {
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1000);
-//         return false;
-//       }
-//     }
-//   });
-// });
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
